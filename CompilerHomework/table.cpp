31ad1fa079c15ac9130453f1047a40df8a9aac83
skynet <erlk0nig@naver.com>
2018-04-05 22:21:44 +0900

    파일 분리 완료

---
+++ CompilerHomework/table.cpp
@@ -0,0 +1,103 @@
+ #include <string>
+#include <vector>
+#include "table_driven.h"
+/* ----------Table -------------*/
+
+using namespace std;
+
+
+Table::Table() {
+	for (int i = 0; i < 11; i++) {
+		Accept[i] = { false };
+	}
+	for (int i = DT_ACCEPT_1_2; i <= DT_ACCEPT_5; i++) {
+		Accept[i] = { true };
+	}
+
+}
+
+Table::Table(vector<SymbolSet> symbolSet){
+
+}
+
+void Table::add_symbol(SymbolSet symbol) {
+	symbolSet.push_back(symbol);
+}
+
+State Table::getNext(State state, string token) {
+
+	int symbol = get_symbolset(token);
+
+	return table[state][symbol];
+}
+State Table::start_state() {
+	return DT_START;
+}
+
+bool Table::is_Accept(State state) {
+	return Accept[state];
+}
+
+int Table::get_symbolset(string input) {
+
+	int size = symbolSet.size();
+
+	for (int i = 0; i < size; i++) {
+		if (symbolSet[i].is_in_set(input)) {
+			return symbolSet[i].get_enumValue();
+		}
+	}
+	return NOT_TOKEN;
+}
+
+/* ---------- SymbolSet-------------*/
+
+void SymbolSet::init(string name, int enumValue) {
+	this->name = name;
+	this->enumValue = enumValue;
+}
+
+SymbolSet::SymbolSet(string name, int enumValue, vector<string> tokens) {
+	init(name, enumValue);
+	this->tokens = tokens;
+	this->type = STRING;
+}
+
+SymbolSet::SymbolSet(string name, int enumValue, int start, int end) {
+	init(name, enumValue);
+	this->start = start;
+	this->end = end;
+	this->type = INTEGER;
+}
+
+bool SymbolSet::is_in_set(string input) {
+	if (type == STRING) {
+
+		int size = tokens.size();
+
+		for (int i = 0; i < size; i++) {
+			if (tokens[i] == input) {
+				return true;
+			}
+		}
+
+	}
+	else if (type == INTEGER) {
+		const char *temp = input.c_str();
+		int t = atoi(temp);
+
+		if (t >= start && t <= end) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+int SymbolSet::get_enumValue() {
+	return enumValue;
+}
+
+string SymbolSet::get_name() {
+	return name;
+}
---
+++ CompilerHomework/table_driven.h
@@ -0,0 +1,111 @@
+#ifndef TABLE_DRIVEN_H
+#define TABLE_DRIVEN_H
+ 
+typedef int State;
+
+class SymbolSet {
+
+private:
+	enum token_type {
+		INTEGER,
+		STRING
+	};
+
+	std::string name;
+	int enumValue;
+
+	/*토큰이 문자나 문자열일 경우*/
+	std::vector<std::string> tokens;
+
+	/*토큰이 일정 범위의 숫자일 경우*/
+	int start;
+	int end;
+
+	int type;
+
+	void init(std::string name, int enumValue);
+
+public:
+
+	SymbolSet(std::string name, int enumValue, std::vector<std::string> tokens);
+
+	SymbolSet(std::string name, int enumValue, int start, int end);
+
+	bool is_in_set(std::string input);
+
+	int get_enumValue();
+
+	std::string get_name();
+};
+
+enum symbols {
+	MONTH,
+	NUM1,
+	NUM2,
+	REST,
+	NOT_TOKEN
+};
+
+enum states {
+	DT_START,
+	DT_MONTH,
+	DT_COM1,
+	DT_COM2,
+	DT_COM3,
+	DT_NUM1,
+	DT_NUM2,
+
+	DT_ACCEPT_1_2,
+	DT_ACCEPT_3,
+	DT_ACCEPT_4,
+	DT_ACCEPT_5,
+};
+
+
+class Table {
+
+private:
+
+	bool Accept[11];
+	std::vector<SymbolSet> symbolSet;
+	
+	const State table[11][5] = {
+		//month     num1     num2      ,         others 
+		{ DT_MONTH, DT_NUM2, DT_START, DT_START, DT_START }, //start
+	{ DT_START, DT_NUM1, DT_NUM1, DT_COM1, DT_START }, //month
+	{ DT_START, DT_NUM1, DT_NUM1, DT_START, DT_START }, //com1
+	{ DT_ACCEPT_4, DT_START, DT_ACCEPT_1_2, DT_ACCEPT_4, DT_START }, //com2
+	{ DT_ACCEPT_3, DT_START, DT_START, DT_START, DT_START },//com3
+	{ DT_ACCEPT_5, DT_ACCEPT_5, DT_ACCEPT_5, DT_COM2, DT_ACCEPT_5 },//num1
+	{ DT_START, DT_START, DT_START, DT_COM3, DT_START },//num2
+
+	{ DT_START, DT_START, DT_START,DT_START, DT_START }, //accpet 1,2
+	{ DT_START, DT_START, DT_START,DT_START, DT_START }, //accept 3
+	{ DT_START, DT_START, DT_START,DT_START, DT_START }, //accept 4
+	{ DT_START, DT_START, DT_START,DT_START, DT_START }, //accept 5
+
+	};
+
+	/* Accept 여부를 확인하기 위한 boolean 배열*/
+
+
+	int get_symbolset(std::string input);
+
+public:
+
+	Table();
+
+	Table(std::vector<SymbolSet> symbolSet);
+
+	void add_symbol(SymbolSet symbol);
+
+	State getNext(State state, std::string token);
+
+	State start_state();
+
+	bool is_Accept(State state);
+};
+
+
+
+#endif
